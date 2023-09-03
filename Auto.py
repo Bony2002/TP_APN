3774328cd37ca3d60f0aae9b0168382cfde58bfc
Bony2002 <valenfbi@gmail.com>
2023-09-03 17:13:18 -0300

    acl

--- Auto.py
+++ Auto.py
@@ -1,16 +1,25 @@
 class Auto:
-    def __init__(self,vel,acl,velmax):
+    def __init__(self,vel,acl,prev,prox,velmax,mdist):
+        self.thw=3
+        self.maxacl=1
+        self.prox=prox
+        self.prev=prev
         self.pos=0
         self.vel=vel
         self.acl=acl
+        self.mindst=mdist
         self.velmax=velmax
         self.nextvel=0
         self.nextacl=0
         self.nextpos=0
-    def decision(self):
-        self.nextpos= self.pos + (self.vel)*1  
-        self.nextvel= self.vel + (self.acl)*1
-        self.nextacl #A definir esto es la decision que hay que tomar (Cuanto acelero o desacelero)
+        self.desiredst=0
+    def decision(self,distraccion):
+        gamma=4
+        self.nextpos= self.pos + (self.vel)*0.5
+        self.nextvel= self.vel + (self.acl)*0.5
+        self.desiredst= self.prox.vel*self.thw 
+        if distraccion:
+            self.nextacl= self.maxacl*[1-(self.vel/self.velmax)**gamma - (self.desiredst/(self.pos-self.prev.pos))**2 ]
         pass
 
     def update(self):
