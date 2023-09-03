d11f28920b204e4f86d1204114a3ea370f906dc9
Bony2002 <valenfbi@gmail.com>
2023-09-03 17:34:39 -0300

    correcion auto

--- Auto.py
+++ Auto.py
@@ -1,10 +1,10 @@
 class Auto:
-    def __init__(self,vel,acl,prev,prox,velmax,mdist):
+    def __init__(self,vel,acl,adelante,atras,velmax,mdist):
         self.thw=3
         self.dt=0.5
         self.maxacl=1
-        self.prox=prox
-        self.prev=prev
+        self.atras=atras
+        self.adelante=adelante
         self.pos=0
         self.vel=vel
         self.acl=acl
@@ -14,15 +14,16 @@ class Auto:
         self.nextacl=0
         self.nextpos=0
         self.desiredst=0
-        self.gap=self.prox.pos - self.pos -4
-    def decision(self,distraccion):
+        self.gap=self.adelante.pos - self.pos -4
+        self.distraction=False
+    def decision(self):
         gamma=4
-        self.desiredst= self.prox.vel*self.thw 
-        if distraccion:
-            self.nextacl= self.maxacl*[1-(self.vel/self.velmax)**gamma - (self.desiredst/(self.gap))**2 ]
+        self.desiredst= self.adelante.vel*self.thw 
+        if self.distraction==False:
+            self.nextacl= self.maxacl*(1-(self.vel/self.velmax)**gamma - (self.desiredst/(self.gap))**2)
         self.nextpos= self.pos + (self.vel)*self.dt
         self.nextvel= self.vel + (self.acl)*self.dt
-        self.newgap= self.prox.pos - self.pos -4
+        self.newgap= self.adelante.pos - self.pos -4
 
 
     def update(self):
